58589cc52a263e5fdc2999a8541dec4b8982ee2e
juan <carlos3434@hotmail.com>
2017-08-05 20:15:03 -0500

    ad

--- webpack.mix.js
+++ webpack.mix.js
@@ -4,8 +4,8 @@ const mix = require('laravel-mix');
 
 mix.babel(
     [
-    'resources/admin/mantenimiento/user_ajax.js',
-    'resources/admin/mantenimiento/user.js'
+    'resources/admin/mantenimiento/users_ajax.js',
+    'resources/admin/mantenimiento/users.js'
     ],
     'public/admin/mantenimiento/user/app.js'
 ).version();
